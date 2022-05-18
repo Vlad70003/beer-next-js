0c80a33ec762c00fd2d7c6e297746f41bc717493
Владислав Порохин <porohin1995@gmail.com>
2022-05-18 15:18:49 +0300

    fix

--- components/Vacancy/Vacancy.tsx
+++ components/Vacancy/Vacancy.tsx
@@ -21,7 +21,6 @@ export const Vacancy = ({
   requirement,
   timetable,
 }: vacancyState) => {
-
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -33,75 +32,98 @@ export const Vacancy = ({
 
   return (
     <>
-    <div className={style.vacancy}>
-      <h3 className={style.vacancy__title}>{title}</h3>
-      <div className={style.vacancy__header}>
-        <div className={style.vacancy__header__leftSide}>
-          <ul className={style.vacancy__header__leftSide__list}>
-            <li className={style.vacancy__header__leftSide__item}>{`Опыт работы от: ${experience}`}</li>
-            <li className={style.vacancy__header__leftSide__item}>{`з/п: ${salary}`}</li>
-            <li className={style.vacancy__header__leftSide__item}>{city}</li>
-          </ul>
-          <ul className={style.vacancy__header__leftSide__list}>
-            <li className={style.vacancy__header__leftSide__requirement}>
-              {requirement}
-            </li>
-            <li className={style.vacancy__header__leftSide__timetable}>
-              {timetable}
-            </li>
-          </ul>
+      <div className={style.vacancy}>
+        <h3 className={style.vacancy__title}>{title}</h3>
+        <div className={style.vacancy__header}>
+          <div className={style.vacancy__header__leftSide}>
+            <ul className={style.vacancy__header__leftSide__list}>
+              <li
+                className={style.vacancy__header__leftSide__item}
+              >{`Опыт работы от: ${experience}`}</li>
+              <li
+                className={style.vacancy__header__leftSide__item}
+              >{`з/п: ${salary}`}</li>
+              <li className={style.vacancy__header__leftSide__item}>{city}</li>
+            </ul>
+            <ul className={style.vacancy__header__leftSide__list}>
+              <li className={style.vacancy__header__leftSide__requirement}>
+                {requirement}
+              </li>
+              <li className={style.vacancy__header__leftSide__timetable}>
+                {timetable}
+              </li>
+            </ul>
+          </div>
+          <div className={style.vacancy__header__rightSide}>
+            <Button
+              title="Откликнуться"
+              color="#3D69B7"
+              border="1px solid #3D69B7"
+              borderRadius="60px"
+              padding="12px 40px"
+              onClick={() => openModalAction("vacancy")}
+            />
+          </div>
         </div>
-        <div className={style.vacancy__header__rightSide}>
-          <Button
-            title="Откликнуться"
-            color="#3D69B7"
-            border="1px solid #3D69B7"
-            borderRadius="60px"
-            padding="12px 40px"
-            onClick={() => openModalAction("vacancy")}
-          />
+        <div className={style.vacancy__footer}>
+          {!isOpen && (
+            <Button
+              title={"Подробнее"}
+              color="#3D69B7"
+              fontWeight="700"
+              margin="16px 0"
+              onClick={() => handleClick()}
+            />
+          )}
+          <div className={style.vacancy__footer__display}>
+            <Button
+              title="Откликнуться"
+              color="#3D69B7"
+              border="1px solid #3D69B7"
+              borderRadius="60px"
+              padding="12px 40px"
+              onClick={() => openModalAction("vacancy")}
+            />
+          </div>
         </div>
-      </div>
-      { !isOpen && <Button
-        title={"Подробнее"}
-        color="#3D69B7"
-        fontWeight="700"
-        margin="16px 0"
-        onClick={() => handleClick()}
-      />}
-      <div
-        className={
-          isOpen
-            ? `${style.vacancy__requirementWrapper} ${style.vacancy__open}`
-            : `${style.vacancy__requirementWrapper} ${style.vacancy__close}`
-        }
-      >
-        <Сonditions title="Условия:" content={conditions} />
-        <Сonditions title="Требования:" content={requirements} />
-      </div>
-      {isOpen && <Button
-        title={"Свернуть"}
-        color="#3D69B7"
-        fontWeight="700"
-        margin="16px 0"
-        onClick={() => handleClick()}
-      />}
-    </div>
 
-    {modal.typeModal === "vacancy" && <ModalWrapper
-        padding="32px 54px"
-        borderRadius="20px"
-        top="50%"
-        left="50%"
-        minWidth="812px"
-        modalIsOpen={modal.modalOpen}
-        backgroundColor="#0000004D"
-        transform="translate(-50%, -50%)"
-        onRequestClose
-        close
-      >
-        < ApplyForJob />
-      </ModalWrapper>}
+        <div
+          className={
+            isOpen
+              ? `${style.vacancy__requirementWrapper} ${style.vacancy__open}`
+              : `${style.vacancy__requirementWrapper} ${style.vacancy__close}`
+          }
+        >
+          <Сonditions title="Условия:" content={conditions} />
+          <Сonditions title="Требования:" content={requirements} />
+        </div>
+        {isOpen && (
+          <Button
+            title={"Свернуть"}
+            color="#3D69B7"
+            fontWeight="700"
+            margin="16px 0"
+            onClick={() => handleClick()}
+          />
+        )}
+      </div>
+
+      {modal.typeModal === "vacancy" && (
+        <ModalWrapper
+          padding="32px 54px"
+          borderRadius="20px"
+          top="50%"
+          left="50%"
+          minWidth="812px"
+          modalIsOpen={modal.modalOpen}
+          backgroundColor="#0000004D"
+          transform="translate(-50%, -50%)"
+          onRequestClose
+          close
+        >
+          <ApplyForJob />
+        </ModalWrapper>
+      )}
     </>
   );
 };
